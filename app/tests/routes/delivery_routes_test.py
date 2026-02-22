9935839147e22dabf7992889eb2941810575ea1a
Jakub Szrajner <19kuperson16@gmail.com>
2026-02-22 16:57:01 +0100

    improved delivery_routes_test

--- app/tests/routes/delivery_routes_test.py
+++ app/tests/routes/delivery_routes_test.py
@@ -283,7 +283,6 @@ def test_create_supplier_delivery_document(
     )
     response_data = response.json()
     exist = deliver_service.supplier_exist(company_name)
-    deliver_service.create_supplier_deliver(company_name, external_number, delivery_date)
     if exist:
         assert response.status_code == 200
         assert response_data["message"] == "Delivery add to database with number PZ-001-02-2026"
